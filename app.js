dad93959d0d5cc6a78a1ed402625a3a8776b7055
Kevin Poston <kposton@qualtrax.com>
2014-12-05 13:43:19 -0500

    adding another directive

--- app.js
+++ app.js
@@ -8,10 +8,17 @@
 var app = angular.module("myApp", []);
 
 app.directive("expandableSection", function() {
-  return function($scope, $element) {
-    $element.find('article').hide();
-    $element.find('h2').click(function() {
-      $element.find('article').toggle();
-    });
+  return {
+    restrict: 'E',
+    template: '<h2>{{title}}</h2><article>{{body}}</article>',
+    scope: true,
+    link: function($scope, $element, attrs) {
+      $scope.title = attrs.sectionTitle;
+      $scope.body = attrs.sectionBody;
+      $element.find('article').hide();
+      $element.find('h2').click(function() {
+        $element.find('article').toggle();
+      });
+    }
   };
 });
