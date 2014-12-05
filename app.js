849916e9c261afd2d0d0b76a0967525233f25126
Kevin Poston <kposton@qualtrax.com>
2014-12-05 13:49:38 -0500

    isolated scope

--- app.js
+++ app.js
@@ -11,10 +11,11 @@ app.directive("expandableSection", function() {
   return {
     restrict: 'E',
     template: '<h2>{{title}}</h2><article>{{body}}</article>',
-    scope: true,
+    scope: {
+      title: '@sectionTitle',
+      body: '@sectionBody'
+    },
     link: function($scope, $element, attrs) {
-      $scope.title = attrs.sectionTitle;
-      $scope.body = attrs.sectionBody;
       $element.find('article').hide();
       $element.find('h2').click(function() {
         $element.find('article').toggle();
