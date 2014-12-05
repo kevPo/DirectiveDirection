33231f6a849d11ffd94ec69bacdb2e6da7a6997d
Kevin Poston <kposton@qualtrax.com>
2014-12-05 13:29:48 -0500

    using angular now

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-$(function() {
-  $('[expandable-selection] article').hide();
-  $('[expandable-selection] h2').click(function() {
-    $(this).parent().find('article').toggle();
-  });
+// $(function() {
+//   $('[expandable-selection] article').hide();
+//   $('[expandable-selection] h2').click(function() {
+//     $(this).parent().find('article').toggle();
+//   });
+// });
+
+var app = angular.module("myApp", []);
+
+app.directive("expandableSection", function() {
+  return function($scope, $element) {
+    $element.find('article').hide();
+    $element.find('h2').click(function() {
+      $element.find('article').toggle();
+    });
+  };
 });
